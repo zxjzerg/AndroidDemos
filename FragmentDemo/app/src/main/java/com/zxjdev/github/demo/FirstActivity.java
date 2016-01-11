90e5ec3c51c578c9c72ebecf1e42bdc9c0d58c13
赵骁俊 <zxjzerg@163.com>
2016-01-11 15:42:56 +0800

    增加Activity

---
+++ FragmentDemo/app/src/main/java/com/zxjdev/github/demo/FirstActivity.java
@@ -0,0 +1,16 @@
+package com.zxjdev.github.demo;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by Andrew on 1/10/16.
+ */
+public class FirstActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+}
--- FragmentDemo/app/src/main/java/com/zxjdev/github/demo/MainActivity.java
+++ FragmentDemo/app/src/main/java/com/zxjdev/github/demo/MainActivity.java
@@ -1,13 +1,29 @@
 package com.zxjdev.github.demo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_first).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_first:
+                Intent first = new Intent(MainActivity.this, FirstActivity.class);
+                startActivity(first);
+                break;
+            default:
+                break;
+        }
     }
 }
